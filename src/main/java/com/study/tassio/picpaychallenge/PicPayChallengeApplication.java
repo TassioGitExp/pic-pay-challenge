edf21ab449999ffe2525ab5f6727f83101fa784d
TassioGitExp <tassio2077@gmail.com>
2023-11-10 22:12:40 -0300

    initial commit

---
+++ src/main/java/com/study/tassio/picpaychallenge/PicPayChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.study.tassio.picpaychallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicPayChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicPayChallengeApplication.class, args);
+	}
+
+}
